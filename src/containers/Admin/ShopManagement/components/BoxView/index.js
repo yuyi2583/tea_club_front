fe4bc09afc8f23103a784eb7e98daa263b58a11f
wuyuding <dinjet@foxmail.com>
2019-12-09 12:26:11 +0800

    '包厢修改'

--- src/containers/Admin/ShopManagement/components/BoxView/index.js
+++ src/containers/Admin/ShopManagement/components/BoxView/index.js
@@ -1,62 +1,195 @@
 import React from "react";
-import { Descriptions, TimePicker } from "antd";
+import { Descriptions, TimePicker, Input, Select, InputNumber, Empty, Row, Col, Button } from "antd";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { actions as shopActions, getBoxes, getShopList } from "../../../../../redux/modules/shop";
+import { actions as shopActions, getBoxes, getShopList, getShop } from "../../../../../redux/modules/shop";
 import moment from 'moment';
 import PictureCard from "../../../../../components/PictureCard";
 import { actions as uiActions, getAlterInfoState } from "../../../../../redux/modules/ui";
+import { getRequestQuantity } from "../../../../../redux/modules/app";
 
 const format = 'HH:mm';
+const { Option } = Select;
+const { TextArea } = Input;
 
 class BoxView extends React.Component {
-    state = {
-        value: moment('12:08', format),
-    };
-    onChange = time => {
-        console.log(time);
-        this.setState({ value: time });
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+
+    componentDidMount() {
+        const { byBoxes, match } = this.props;
+        const { boxId } = match.params;
+        this.setState({ ...byBoxes[boxId] });
+    }
+
+    handleChange = (e) => {
+        const name = e.target.name;
+        this.setState({
+            [name]: e.target.value
+        })
+    }
+
+    // handleSelectChange=(value)=>{
+    //     this.setState({})
+    // }
+
+    // getSelectChildren=()=>{
+    //     const {byShopList,shop}=this.props;
+    //     const selectChildren=shop.shopList.map((shopId)=>(
+    //         <Option value={shopId}>{byShopList[shopId].name}</Option>
+    //     ))
+    //     return selectChildren;
+    // }
+
+    handleInputNumChange = (value) => {
+        this.setState({ price: value })
+    }
+
+    handleStartTimePickerChange = (time, timeString) => {
+        this.setState({ startTime: timeString })
+    }
+
+    handleEndTimePickerChange = (time, timeString) => {
+        this.setState({ endTime: timeString })
+    }
+
+    forDisplay = () => {
+        const { byBoxes, match } = this.props;
+        const { boxId } = match.params;
+        if (byBoxes[boxId] === null || byBoxes[boxId].img === null) {
+            return {
+                fileListInProps: new Array(),
+                fileListInState: new Array()
+            };
+        }
+        const fileListInProps = byBoxes[boxId].img;
+        const fileListInState = this.state.img == null ? new Array() : this.state.img;
+        return {
+            fileListInProps,
+            fileListInState
+        };
+    }
+
+    handleDisplayChange = (fileList) => {
+        // let img = new Array();
+        // for (var key in fileList) {
+        //     img.push(fileList[key].uid);
+        // }
+        this.setState({ img: fileList });
+        // this.props.setDisplay(display);
     };
 
+    completeAlter = () => {
+        const newBoxInfo=this.state;
+        const {match}=this.props;
+        const {shopId,boxId}=match.params;
+        //TODO 修改store中的byBoxes
+    }
+
+    handleCancelAlter = () => {
+        const { byBoxes, match } = this.props;
+        const { boxId } = match.params;
+        this.setState({ ...byBoxes[boxId] });
+        this.props.finishAlterInfo();
+    }
+
+    componentWillUnmount(){
+        this.props.finishAlterInfo();
+    }
 
     render() {
-        const { match, byBoxes, byShopList,alterInfo } = this.props;
+        const { match, byBoxes, byShopList, alterInfo, requestQuantity } = this.props;
         const { shopId, boxId } = match.params;
-        const fileList = byBoxes == null ? [] : byBoxes[boxId].img;
+        const { fileListInProps, fileListInState } = this.forDisplay();
+        // const selectChildren=this.getSelectChildren();
         return (
             <div>
                 <Descriptions bordered column={2}>
                     <Descriptions.Item label="包厢名称">
-                        {byBoxes[boxId].name}
+                        {!alterInfo ?
+                            byBoxes[boxId].name
+                            : <Input value={this.state.name} allowClear name="name" onChange={this.handleChange} />
+                        }
                     </Descriptions.Item>
                     <Descriptions.Item label="包厢号">
-                        {byBoxes[boxId].boxNum}
+                        {!alterInfo ?
+                            byBoxes[boxId].boxNum
+                            : <Input value={this.state.boxNum} allowClear name="boxNum" onChange={this.handleChange} />
+                        }
                     </Descriptions.Item>
                     <Descriptions.Item label="所属门店">
-                        {byShopList[shopId].name}
+                        {/* {!alterInfo ? */}
+                        {byShopList[shopId].name
+                            // : <Select
+                            //     showSearch
+                            //     style={{ width: 200 }}
+                            //     placeholder="请选择门店"
+                            //     defaultValue={shopId}
+                            //     optionFilterProp="children"
+                            //     onChange={this.handleSelectChange}
+                            //     filterOption={(input, option) =>
+                            //         option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                            //     }
+                            // >
+                            //     {selectChildren}
+                            // </Select>
+                        }
                     </Descriptions.Item>
                     <Descriptions.Item label="费用">
-                        {byBoxes[boxId].price}/小时
+                        {!alterInfo ?
+                            byBoxes[boxId].price + "/小时"
+                            : <InputNumber
+                                defaultValue={this.state.price}
+                                min={0}
+                                onChange={this.handleInputNumChange}
+                            />
+                        }
                     </Descriptions.Item>
                     <Descriptions.Item label="开放时间" span={2}>
-                        {byBoxes[boxId].startTime}~{byBoxes[boxId].endTime}
-                        {/* <TimePicker
-                            defaultValue={moment('12:08', format)}
-                            value={this.state.value}
-                            format={format}
-                            onChange={this.onChange}
-                        /> */}
+                        {!alterInfo ?
+                            byBoxes[boxId].startTime + " ~ " + byBoxes[boxId].endTime
+                            : <div>
+                                <TimePicker value={moment(this.state.startTime, format)} format={format} onChange={this.handleStartTimePickerChange} />
+                                &nbsp; ~ &nbsp;
+                                <TimePicker value={moment(this.state.endTime, format)} format={format} onChange={this.handleEndTimePickerChange} />
+                            </div>
+                        }
                     </Descriptions.Item>
                     <Descriptions.Item label="包厢介绍" span={2}>
-                        {byBoxes[boxId].description}
+                        {!alterInfo ?
+                            byBoxes[boxId].description
+                            : <TextArea rows={4} name="description" onChange={this.handleChange} allowClear value={this.state.description} />
+                        }
                     </Descriptions.Item>
                     <Descriptions.Item label="包厢照片" span={2}>
-                        <PictureCard
-                            fileList={fileList}
-                            alterInfo={alterInfo}
-                            onChange={this.handleDisplayChange} />
+                        {alterInfo ?
+                            <PictureCard
+                                fileList={fileListInState}
+                                state
+                                alterInfo={alterInfo}
+                                onChange={this.handleDisplayChange} />
+                            : fileListInProps.length == null || fileListInProps == 0 ?
+                                <Empty />
+                                : <PictureCard
+                                    props
+                                    fileList={fileListInProps}
+                                    alterInfo={alterInfo} />
+                        }
                     </Descriptions.Item>
                 </Descriptions>
+                {alterInfo ?
+                    <Row style={{ margin: "20px 0" }}>
+                        <Col span={12} offset={4}>
+                            <Button type="primary" block onClick={this.completeAlter} loading={requestQuantity > 0} >{requestQuantity > 0 ? "" : "确认修改"}</Button>
+                        </Col>
+                        <Col span={4} push={4}>
+                            <Button block onClick={this.handleCancelAlter}>取消修改</Button>
+                        </Col>
+                    </Row>
+                    : null}
             </div>
         )
     }
@@ -66,7 +199,8 @@ const mapStateToProps = (state, props) => {
     return {
         byBoxes: getBoxes(state),
         byShopList: getShopList(state),
-        alterInfo: getAlterInfoState(state),
+        shop: getShop(state),
+        requestQuantity: getRequestQuantity(state),
     };
 };
 
--- src/containers/Admin/ShopManagement/index.js
+++ src/containers/Admin/ShopManagement/index.js
@@ -7,6 +7,7 @@ import ShopView from "./components/ShopView";
 import { Route, Link } from "react-router-dom";
 import {
     actions as uiActions,
+    getAlterInfoState
 } from "../../../redux/modules/ui";
 import ClerkView from "./components/ClerkView";
 import AddShop from "./components/AddShop";
@@ -18,19 +19,31 @@ class ShopManagement extends React.Component {
         window.history.back();
     }
 
+    getExtra = () => {
+        const { history,match } = this.props;
+        let extra = null;
+        if(history.location.pathname === "/administrator/company/shop_management"){
+            extra = (
+                <Link to={`${match.url}/addShop`}>
+                    <Button type="primary">新增门店</Button>
+                </Link>);
+        }else if(history.location.pathname.indexOf("boxInfo")!=-1){
+            extra=(<Button type="primary" onClick={()=>this.props.startAlterInfo()}>修改包厢信息</Button>);
+        }else{
+            extra=null;
+        }
+        return extra;
+    }
 
     render() {
-        const { byClerks, match, addButtonVisible, history } = this.props;
+        const { byClerks, match, addButtonVisible, history,alterInfo } = this.props;
+        const extra = this.getExtra();
         return (
             <div>
                 <PageHeader
                     title="门店管理"
                     onBack={this.handleBack}
-                    extra={history.location.pathname != "/administrator/company/shop_management" ? null :
-                        <Link to={`${match.url}/addShop`}>
-                            <Button type="primary">新增门店</Button>
-                        </Link>
-                    }>
+                    extra={extra}>
                     <div>
                         <Route
                             path={match.url}
@@ -45,7 +58,9 @@ class ShopManagement extends React.Component {
                         <Route
                             path={`${match.url}/boxInfo/:shopId/:boxId`}
                             render={props =>
-                                <BoxView {...props} />
+                                <BoxView 
+                                    {...props} 
+                                    alterInfo={alterInfo}/>
                             } />
                         <Route
                             path={`${match.url}/clerkDetail/:shopId/:clerkId`}
@@ -62,6 +77,7 @@ class ShopManagement extends React.Component {
 const mapStateToProps = (state, props) => {
     return {
         byClerks: getClerks(state),
+        alterInfo:getAlterInfoState(state)
     };
 };
 
